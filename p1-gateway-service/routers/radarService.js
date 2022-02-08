1887012ac5ece3eb8fa5b51081c77a6486f4255a
Amol Sangar <amolsangar.c@gmail.com>
2022-02-08 01:51:05 -0500

    #13

--- p1-gateway-service/routers/radarService.js
+++ p1-gateway-service/routers/radarService.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 var config = require(path.join(__dirname,'../config.js')).get(process.env.NODE_ENV);
 const AxiosWrapper = require(path.join(__dirname,".././apiHandler/AxiosWrapper.js"))
 const asyncHandler = require('express-async-handler')
+const request = require('request');
 
 var router = express.Router()
 
@@ -16,8 +17,16 @@ const radarAPI = AxiosWrapper(radarService);
 // SERVICE ROUTING
 router.get('/radar/plot', asyncHandler(async(req, res) => {
     // /radar/plot?radar_id=KAMX&date=10-10-2020&hour=15
-    let resp = await radarAPI.get(req.url)
-    res.send(resp.data)
+    request({
+        url: radarService+req.url,
+        encoding: null
+    }, 
+    (err, resp, buffer) => {
+    if (!err && resp.statusCode === 200){
+        res.set("Content-Type", "image/png");
+        res.send(resp.body);
+    }
+    });
 }))
 
 // =======================================================
